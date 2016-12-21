82a38a816b97ac751bc6ba7a305a001c84eb94e2
Cas Eliëns <cas.eliens@gmail.com>
2016-12-21 14:11:13 +0100

    Add user join and leave messages

--- gulpfile.js
+++ gulpfile.js
@@ -10,53 +10,66 @@ let plumber = require('gulp-plumber');
 let coveralls = require('gulp-coveralls');
 
 gulp.task('static', function () {
-  return gulp.src('**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(eslint())
-    .pipe(eslint.format());
+    return gulp.src('**/*.js')
+        .pipe(excludeGitignore())
+        .pipe(eslint())
+        .pipe(eslint.format());
     //.pipe(eslint.failAfterError());
 });
 
 gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
+    nsp({package: path.resolve('package.json')}, cb);
 });
 
 gulp.task('pre-test', function () {
-  return gulp.src('lib/**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(istanbul({
-      includeUntested: true
-    }))
-    .pipe(istanbul.hookRequire());
+    return gulp.src('lib/**/*.js')
+        .pipe(excludeGitignore())
+        .pipe(istanbul({
+            includeUntested: true
+        }))
+        .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', ['pre-test'], function (cb) {
-  let mochaErr;
+    let mochaErr;
 
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
-    .on('error', function (err) {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', function () {
-      cb(mochaErr);
-    });
+    gulp.src('test/**/*.js')
+        .pipe(plumber())
+        .pipe(mocha({reporter: 'spec'}))
+        .on('error', function (err) {
+            mochaErr = err;
+        })
+        .pipe(istanbul.writeReports())
+        .on('end', function () {
+            cb(mochaErr);
+        });
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
+    gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
 });
 
 gulp.task('coveralls', ['test'], function () {
-  if (!process.env.CI) {
-    return;
-  }
+    if (!process.env.CI) {
+        return;
+    }
 
-  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls());
+    return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+        .pipe(coveralls());
+});
+
+gulp.task('codacy', ['coveralls'], function codacyTask() {
+    if(!process.env.CI) {
+        return;
+    }
+
+    return gulp
+        .src(['coverage/lcov.info'])
+        .pipe(codacy({
+            token: process.env.CODACYTOKEN
+        }));
 });
 
 gulp.task('prepublish', ['nsp']);
 gulp.task('default', ['static', 'test', 'coveralls']);
+gulp.task('coverage', ['test', 'coveralls', 'codacy']);
