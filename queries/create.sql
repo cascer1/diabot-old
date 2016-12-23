317bc351e07a7c36154bda763f5a7b4dc1c63dea
Cas Eliëns <cas.eliens@gmail.com>
2016-12-23 12:58:25 +0100

    Improve user and server registration

--- queries/create.sql
+++ queries/create.sql
@@ -1,10 +1,31 @@
+/*
+==== SERVERS ====
+ */
+
+CREATE TABLE `servers` (
+  `id`   VARCHAR(32)
+         COLLATE utf8_unicode_ci NOT NULL,
+  `name` VARCHAR(256)
+         COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
+
+/*
+==== USERS ====
+ */
+
 CREATE TABLE `users` (
   `id`       VARCHAR(32)
              COLLATE utf8_unicode_ci NOT NULL,
   `serverId` VARCHAR(32)
              COLLATE utf8_unicode_ci NOT NULL,
   `type`     VARCHAR(45)
-             CHARACTER SET utf8 DEFAULT 'NONE',
+             CHARACTER SET utf8      NOT NULL DEFAULT 'NONE',
+  `role`     VARCHAR(16)
+             COLLATE utf8_unicode_ci NOT NULL DEFAULT 'USER',
   PRIMARY KEY (`id`, `serverId`),
   KEY `server_user_idx` (`serverId`),
   CONSTRAINT `server_user` FOREIGN KEY (`serverId`) REFERENCES `servers` (`id`)
@@ -16,14 +37,24 @@ CREATE TABLE `users` (
   COLLATE = utf8_unicode_ci;
 
 
-CREATE TABLE IF NOT EXISTS `rates` (
+/*
+==== RATES ====
+ */
+
+CREATE TABLE `rates` (
   `userId`    VARCHAR(32)
+              COLLATE utf8_unicode_ci NOT NULL,
+  `serverId`  VARCHAR(32)
               COLLATE utf8_unicode_ci NOT NULL,
   `type`      VARCHAR(10)
               COLLATE utf8_unicode_ci NOT NULL,
   `startTime` INT(2)                  NOT NULL,
   `rate`      DECIMAL(5, 2)           NOT NULL,
-  PRIMARY KEY (`userId`, `type`, `startTime`),
+  PRIMARY KEY (`userId`, `serverId`, `type`, `startTime`),
+  KEY `rate_server_idx` (`serverId`),
+  CONSTRAINT `rate_server` FOREIGN KEY (`serverId`) REFERENCES `servers` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `user_rates` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
@@ -32,29 +63,19 @@ CREATE TABLE IF NOT EXISTS `rates` (
   DEFAULT CHARSET = utf8
   COLLATE = utf8_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `servers` (
-  `id`      VARCHAR(32)
-            COLLATE utf8_unicode_ci NOT NULL,
-  `name`    VARCHAR(256)
-            COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ownerId` VARCHAR(32)
-            COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `server_owner_idx` (`ownerId`),
-  CONSTRAINT `server_owner` FOREIGN KEY (`ownerId`) REFERENCES `users` (`id`)
-    ON UPDATE CASCADE
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
-  COLLATE = utf8_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `settings` (
+/*
+==== SETTINGS ====
+ */
+
+CREATE TABLE `settings` (
   `serverId` VARCHAR(32)
              COLLATE utf8_unicode_ci NOT NULL,
   `setting`  VARCHAR(45)
              COLLATE utf8_unicode_ci NOT NULL,
   `value`    VARCHAR(256)
              COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`serverId`, `setting`),
   UNIQUE KEY `server_setting_primary` (`serverId`, `setting`),
   KEY `serverId_idx` (`serverId`),
   CONSTRAINT `server_settings` FOREIGN KEY (`serverId`) REFERENCES `servers` (`id`)
@@ -64,6 +85,11 @@ CREATE TABLE IF NOT EXISTS `settings` (
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
   COLLATE = utf8_unicode_ci;
+
+
+/*
+==== TYPES ====
+ */
 
 CREATE TABLE `types` (
   `serverId` VARCHAR(32)
@@ -78,4 +104,5 @@ CREATE TABLE `types` (
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
   COLLATE = utf8_unicode_ci;
+
 
