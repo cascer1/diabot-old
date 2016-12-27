47f61f4daf4c15d8c3188f4c7a838328cb6e58d7
Cas Eliëns <cas.eliens@gmail.com>
2016-12-27 13:35:33 +0100

    Fix Codacy issues

--- test/index.js
+++ test/index.js
@@ -17,16 +17,14 @@
  *
  */
 
-'use strict';
+"use strict";
 
-let assert = require('assert');
-// var diabot = require('../lib');
+let assert = require("assert");
 let functions = require("../lib/functions.js");
 
 
-
-describe('fixture', () => {
-  it('can convert mmol/L to mg/dL', () => {
+describe("fixture", () => {
+  it("can convert mmol/L to mg/dL", () => {
     let input = 5.5;
     let expected = 99.1;
     let actual = functions.convertToMgdl(input);
